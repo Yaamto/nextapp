6123a22de865baa6420b565c6cf524c9b7c4a082
Yaamto <73040102+Yaamto@users.noreply.github.com>
2023-05-05 19:32:48 +0200

    Home page done

--- src/fonts/font.tsx
+++ src/fonts/font.tsx
@@ -1,3 +1,9 @@
 import { Oswald } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 
 export const oswald = Oswald({ subsets: ['latin'] });
+
+export const roboto = Roboto({
+     subsets: ['latin'],
+     weight: '400', 
+    });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,29 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { oswald } from '@/fonts/font'
+import Discover from '@/components/Discover'
+import Strategie from '@/components/Strategie'
 
 
 
 export default function Home() {
   return (
+    <div>
+
     <div className='header'>
-    <div className='flex pt-[150px] justify-around'> 
-      <div className='flex flex-col items-start ml-[350px]'>
+    <div className='md:flex pt-[150px] md:justify-around items-center'> 
+      <div className='flex flex-col text-center md:text-start items-center md:items-start justify-center md:justify-start lg:ml-[200px] md:ml-[100px]  '>
         <h1 className='text-[64px] font-bold'>SchoolFPS</h1>
-        <p className='text-[40px] lg:w-2/3 w-100'>Learn everything about utilities and strategies on Counter Strike.</p>
+        <p className='text-[40px] w-100'>Learn everything about utilities and strategies on Counter Strike.</p>
         <button className='dark:bg-white dark:text-dmode bg-dmode text-white text-[23px] px-5 py-1 rounded mt-5'>Getting Started</button>
       </div>
-      <Image src="/cs-hero.png" width={800} height={700} alt="" className='mr-[150px]'/>
+      <div className='lg:mr-[150px]  md:w-50 xl:w-100 hidden md:block'>
+        <Image src="/cs-hero.png" width={800} height={700} alt="" className=''/>
+      </div>
     </div>
-   
-      {/* <div className='w-full h-[300px] dark:bg-[url("/dwave.svg")] bg-no-repeat absolute top-[580px] left-0'>
-
-      </div> */}
+    </div>
+    <Strategie />
+    <Discover />
     </div>
   )
 }
